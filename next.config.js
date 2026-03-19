f0d8d7e009d05735d8eaa45de7dc47d4b306be62
hamzahayatunstuck <hamza.hayat@unstucklabs.com>
2026-03-19 20:22:35 +0500

    Add next.config.js

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    serverActions: true,
+  },
+};
+
+module.exports = nextConfig;
